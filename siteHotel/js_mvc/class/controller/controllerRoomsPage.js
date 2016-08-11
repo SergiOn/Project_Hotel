d39950719f55e7872f01b301f542539a468bf71c
Sergio Onischenko <Onishhenko@gmail.com>
2016-08-11 15:22:11 +0300

    make controllerRoomsPage

--- siteHotel/js_mvc/class/controller/controllerRoomsPage.js
+++ siteHotel/js_mvc/class/controller/controllerRoomsPage.js
@@ -6,12 +6,15 @@ function controllerRoomsPage(controllerPage) {
     this.controllerPage = controllerPage;
     this.model = new modelRoomsPage();
     this.view = new viewRoomsPage();
+
+    this.reservObject = {};
 }
 
 
 controllerRoomsPage.prototype.controllerInit = function () {
     this.start();
     this.startCalendar();
+    this.changeSearch();
 };
 
 controllerRoomsPage.prototype.start = function () {
@@ -55,18 +58,165 @@ controllerRoomsPage.prototype.start = function () {
 };
 
 controllerRoomsPage.prototype.startCalendar = function () {
-    var dayIn,
-        monthIn,
-        yearIn,
-        countMoIn = 0,
-        dayOut,
-        monthOut,
-        yearOut,
-        countMoOut = 0,
-        objData;
+    var self = this,
+        monthNameObj = ['Января', 'Февраля', 'Марта', 'Апреля', 'Майя', 'Июня', 'Июля', 'Августа', 'Сентября', 'Октября', 'Ноября', 'Декабря'],
+        monthNameIn, monthNameOut,
+        div, elTarg, mon, day, calendar,
+        dayIn, monthIn, yearIn, countMoIn = 0, innerIn, dataIn,
+        dayOut, monthOut, yearOut, countMoOut = 0, innerOut, dataOut;
 
+    document.querySelector('.room_cal-in').addEventListener('click', function (event) {
+        div = event.currentTarget;
+        elTarg = event.target;
 
+        if (!div.querySelector('table.calendar')) {
+            calendar = self.controllerPage.calendarRooms(dayIn, monthIn, yearIn, countMoIn);
+            div.appendChild(calendar);
+        } else if (div.querySelector('table.calendar') && elTarg.closest('button')) {
+            div.removeChild(calendar);
+            return;
+        }
 
+        if (elTarg.matches('td:not(.no)') && !elTarg.closest('thead')) {
+            dayIn = parseInt(elTarg.innerHTML);
+            monthIn = parseInt(div.querySelector('.month_cal').dataset.month);
+            yearIn = parseInt(div.querySelector('.month_cal').dataset.year);
+            countMoIn = 0;
+            day = String(dayIn).length > 1 ? dayIn : '0'+dayIn;
+            mon = String(monthIn).length > 1 ? monthIn : '0'+monthIn;
 
+            monthNameIn = monthNameObj[monthIn-1];
+            innerIn = day+' '+monthNameIn+' '+yearIn;
+            dataIn = day+'-'+mon+'-'+yearIn;
 
+            self.view.startCalendar(innerIn, false, dataIn, false);
+            div.removeChild(calendar);
+        } else if (elTarg.closest('.prev_cal')) {
+            countMoIn -= 1;
+            div.removeChild(calendar);
+            calendar = self.controllerPage.calendarRooms(dayIn, monthIn, yearIn, countMoIn);
+            div.appendChild(calendar);
+        } else if (elTarg.closest('.next_cal')) {
+            countMoIn += 1;
+            div.removeChild(calendar);
+            calendar = self.controllerPage.calendarRooms(dayIn, monthIn, yearIn, countMoIn);
+            div.appendChild(calendar);
+        }
+    });
+
+    document.querySelector('.room_cal-out').addEventListener('click', function (event) {
+        div = event.currentTarget;
+        elTarg = event.target;
+
+        if (!div.querySelector('table.calendar')) {
+            calendar = self.controllerPage.calendarRooms(dayOut, monthOut, yearOut, countMoOut);
+            div.appendChild(calendar);
+        } else if (div.querySelector('table.calendar') && elTarg.closest('button')) {
+            div.removeChild(calendar);
+            return;
+        }
+
+        if (elTarg.matches('td:not(.no)') && !elTarg.closest('thead')) {
+            dayOut = parseInt(elTarg.innerHTML);
+            monthOut = parseInt(div.querySelector('.month_cal').dataset.month);
+            yearOut = parseInt(div.querySelector('.month_cal').dataset.year);
+            countMoOut = 0;
+            day = String(dayOut).length > 1 ? dayOut : '0'+dayOut;
+            mon = String(monthOut).length > 1 ? monthOut : '0'+monthOut;
+
+            monthNameOut = monthNameObj[monthOut-1];
+            innerOut = day+' '+monthNameOut+' '+yearOut;
+            dataOut = day+'-'+mon+'-'+yearOut;
+
+            self.view.startCalendar(false, innerOut, false, dataOut);
+            div.removeChild(calendar);
+        } else if (elTarg.closest('.prev_cal')) {
+            countMoOut -= 1;
+            div.removeChild(calendar);
+            calendar = self.controllerPage.calendarRooms(dayOut, monthOut, yearOut, countMoOut);
+            div.appendChild(calendar);
+        } else if (elTarg.closest('.next_cal')) {
+            countMoOut += 1;
+            div.removeChild(calendar);
+            calendar = self.controllerPage.calendarRooms(dayOut, monthOut, yearOut, countMoOut);
+            div.appendChild(calendar);
+        }
+    });
 };
+
+
+controllerRoomsPage.prototype.changeSearch = function () {
+    var self = this,
+        el,
+        calendar,
+        section = document.querySelector('.block-1_rooms_top'),
+        timeIn = document.querySelector('#room_cal-in span').dataset.timeIn,
+        timeOut = document.querySelector('#room_cal-out span').dataset.timeOut,
+        input = document.querySelectorAll('#rooms_sort-block input'),
+        selectSteps = document.querySelector('#rooms_sort-block select[name="steps"]'),
+        selectCount = document.querySelector('#rooms_sort-block select[name="count"]');
+
+    this.reservObject['page'] = 1;
+    this.reservObject['timeIn'] = timeIn;
+    this.reservObject['timeOut'] = timeOut;
+
+    for (var key in input) if (input.hasOwnProperty(key)) {
+        this.reservObject[input[key].name] = Number(input[key].checked);
+    }
+    this.reservObject[selectSteps.name] = selectSteps.value;
+    this.reservObject[selectCount.name] = selectCount.value;
+
+    // запустить ajax
+    console.log(this.reservObject);
+
+
+    document.getElementById('rooms_sort-block').addEventListener('change', function (event) {
+        el = event.target;
+
+        if (el.tagName === 'INPUT') {
+            self.reservObject[el.name] = Number(el.checked);
+        } else if (el.tagName === 'SELECT') {
+            self.reservObject[el.name] = el.value;
+        }
+        self.reservObject['page'] = 1;
+
+        // запустить ajax
+        console.log(self.reservObject);
+    });
+
+
+    section.addEventListener('click', function (event) {
+        calendar = this.querySelector('table.calendar');
+        if (calendar) return;
+
+        timeIn = document.querySelector('#room_cal-in span').dataset.timeIn;
+        timeOut = document.querySelector('#room_cal-out span').dataset.timeOut;
+
+        if (timeIn < timeOut) {
+            self.reservObject['timeIn'] = timeIn;
+            self.reservObject['timeOut'] = timeOut;
+            self.reservObject['page'] = 1;
+
+            // запустить ajax
+            console.log(self.reservObject);
+        }
+    });
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- siteHotel/js_mvc/class/view/viewRoomsPage.js
+++ siteHotel/js_mvc/class/view/viewRoomsPage.js
@@ -7,8 +7,17 @@ function viewRoomsPage() {
 }
 
 viewRoomsPage.prototype.start = function (innerIn, innerOut, dataIn, dataOut) {
-    document.querySelector('#room_cal-in span').innerHTML = innerIn;
-    document.querySelector('#room_cal-out span').innerHTML = innerOut;
-    document.querySelector('#room_cal-in span').dataset.timeIn = dataIn;
-    document.querySelector('#room_cal-out span').dataset.timeOut = dataOut;
+    if (innerIn && dataIn) {
+        document.querySelector('#room_cal-in span').innerHTML = innerIn;
+        document.querySelector('#room_cal-in span').dataset.timeIn = dataIn;
+    }
+    if (innerOut && dataOut) {
+        document.querySelector('#room_cal-out span').innerHTML = innerOut;
+        document.querySelector('#room_cal-out span').dataset.timeOut = dataOut;
+    }
+
+};
+
+viewRoomsPage.prototype.startCalendar = function (innerIn, innerOut, dataIn, dataOut) {
+    this.start(innerIn, innerOut, dataIn, dataOut);
 };
